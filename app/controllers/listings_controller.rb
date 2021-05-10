4bb43975a58c5e31e69c5550d1218b0967e02f63
henrydrayton <henrydrayton0@gmail.com>
2021-05-10 20:45:27 +1000

    add form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,22 @@ class ListingsController < ApplicationController
     def index
         @listings = Listing.all
     end
+
+    # def show 
+    #     @listing = Listing.find(params[:id])
+    # end
+
+    def new
+        @listing = Listing.new
+      end
+    
+      def create
+        @listing = Listing.new(name: "...", price: "...", age: "...", description: "...")
+    
+        if @listing.save
+          redirect_to @listing
+        else
+          render :new
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  root "listings#index"
+
   get "/listings", to: "listings#index"
+  get "/listings/:id", to: "listings#show"
+
 end
