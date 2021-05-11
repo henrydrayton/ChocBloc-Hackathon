fb5fdf3cf6be3a64fde260569c9eab2cb9cdb8f0
Garvey <hello@garvey.codes>
2021-05-11 20:42:07 +1000

    Fix navbar, authentication logic, listing saving

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,52 +1,67 @@
 class ListingsController < ApplicationController
-
+  before_action :require_login, except: :index
+  
     def index
         @listings = Listing.all.reverse
     end
 
     def show 
-        @listing = Listing.find(params[:id])
+      @listing = Listing.find(params[:id])
+    rescue 
+      redirect_to root_path
     end
 
     def new
         @listing = Listing.new
-      end
+    end
     
-      def create
-        @listing = Listing.new(listing_params)
-    
+    def create
+      @listing = Listing.new(listing_params)
+      @listing[:user_id] = current_user.id 
+      
+      respond_to do |format|
         if @listing.save
-          redirect_to @listing
+          format.html { redirect_to @listing, notice: "Listing was successfully created." }
+          format.json { render :show, status: :created, location: @listing }
         else
-          render :new
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @listing.errors, status: :unprocessable_entity }
         end
       end
+    end
 
-      def edit
-        @listing = Listing.find(params[:id])
-      end
+    def edit
+      @listing = Listing.find(params[:id])
+    end
 
-      def update
-        @listing = Listing.find(params[:id])
-    
-        if @listing.update(listing_params)
-          redirect_to @listing
-        else
-          render :edit
-        end
-      end
-
-      def destroy
-        @listing = Listing.find(params[:id])
-        @listing.destroy
-    
-        redirect_to root_path
-      end
-
-
-      private
-      def listing_params
-        params.require(:listing).permit(:name, :price, :age, :description, :image)
-      end
+    def update
+      @listing = Listing.find(params[:id])
   
+      if @listing.update(listing_params)
+        redirect_to @listing
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @listing = Listing.find(params[:id])
+      @listing.destroy
+  
+      redirect_to root_path
+    end
+
+
+  private
+    def require_login
+      unless authenticate_user!
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to new_user_session_path
+      end
+    end
+
+    def listing_params
+      params.require(:listing).permit(:name, :price, :age, :description, :image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
-  root to: 'pages#home'
   devise_for :users
+  
+  root to: 'listings#index'
 
-  # root "listings#index"
-  get 'pages/home'
   resources :listings
 
 end
