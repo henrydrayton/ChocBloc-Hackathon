3320b0bfec08b4cb584a862abea84598ca8b7c87
Garvey <hello@garvey.codes>
2021-05-12 15:28:56 +1000

    Add seller button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/users/:id', to: 'users#profile'
+  get '/users/:id', to: 'users#profile', as: 'user_profile'
   
   root to: 'listings#index'
   resources :listings
