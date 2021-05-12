50e3b1f2123586859cebf749dc486d1c1324e636
Garvey <hello@garvey.codes>
2021-05-12 13:52:01 +1000

    Add individual user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-    
+  before_action :set_user_profile
+
+  def profile
+  end
+
+  private
+    def set_user_profile
+      if User.exists?(params[:id])
+        @user_profile = User.find(params[:id])
+        @user_listings = Listing.where(user_id: @user_profile.id)
+      else
+        redirect_to root_path
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users/:id', to: 'users#profile'
   
   root to: 'listings#index'
-
   resources :listings
-
 end
